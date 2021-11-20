df8a6f41902f8f30a04c94a972edb8edcb1106e1
Mauricio Meza <mmezab@unal.edu.co>
2021-11-20 18:00:59 -0500

    bash con loops

--- src/script_omp.sh
+++ src/script_omp.sh
@@ -4,26 +4,38 @@ EXEC=sobel
 
 gcc -o $EXEC readImage-openmp.c -lm -fopenmp
 
-echo "Sobel con imagen 4K y Kernel de 5x5 - 1 Hilo"
-echo "messi4k.jpg m4k-5.jpg 5 1" | ./$EXEC > out.txt
+for hilos in 1 2 3 4 6 8 10 12 14 16 18 20
+do
+	echo "Sobel con imagen 4K y Kernel de 5x5 - $ Hilo"
+	echo "messi4k.jpg m4k-5.jpg 5 $hilos" | ./$EXEC >> out4k-5.txt
+done
 
-echo "Sobel con imagen 4K y Kernel de 5x5 - 2 Hilo"
-echo "messi4k.jpg m4k-5.jpg 5 2" | ./$EXEC >> out.txt
+for hilos in 1 2 3 4 6 8 10 12 14 16 18 20
+do
+	echo "Sobel con imagen 4K y Kernel de 3x3 - $ Hilo"
+	echo "messi4k.jpg m4k-3.jpg 3 $hilos" | ./$EXEC >> out4k-3.txt
+done
 
-echo "Sobel con imagen 4K y Kernel de 5x5 - 3 Hilo"
-echo "messi4k.jpg m4k-5.jpg 5 3" | ./$EXEC >> out.txt
+for hilos in 1 2 3 4 6 8 10 12 14 16 18 20
+do
+	echo "Sobel con imagen 1080 y Kernel de 5x5 - $ Hilo"
+	echo "messi1080.jpg m1080-5.jpg 5 $hilos" | ./$EXEC >> out1080-5.txt
+done
 
-echo "Sobel con imagen 4K y Kernel de 5x5 - 4 Hilo"
-echo "messi4k.jpg m4k-5.jpg 5 4" | ./$EXEC >> out.txt
+for hilos in 1 2 3 4 6 8 10 12 14 16 18 20
+do
+	echo "Sobel con imagen 1080 y Kernel de 3x3 - $ Hilo"
+	echo "messi1080.jpg m1080-3.jpg 3 $hilos" | ./$EXEC > out1080-3.txt
+done
 
-echo "Sobel con imagen 4K y Kernel de 5x5 - 6 Hilo"
-echo "messi4k.jpg m4k-5.jpg 5 6" | ./$EXEC >> out.txt
+for hilos in 1 2 3 4 6 8 10 12 14 16 18 20
+do
+	echo "Sobel con imagen 720 y Kernel de 5x5 - $ Hilo"
+	echo "messi720.jpg m720-5.jpg 5 $hilos" | ./$EXEC > out720-5.txt
+done
 
-echo "Sobel con imagen 4K y Kernel de 5x5 - 8 Hilo"
-echo "messi4k.jpg m4k-5.jpg 5 8" | ./$EXEC >> out.txt
-
-echo "Sobel con imagen 4K y Kernel de 5x5 - 12 Hilo"
-echo "messi4k.jpg m4k-5.jpg 5 12" | ./$EXEC >> out.txt
-
-echo "Sobel con imagen 4K y Kernel de 5x5 - 16 Hilo"
-echo "messi4k.jpg m4k-5.jpg 5 16" | ./$EXEC >> out.txt
+for hilos in 1 2 3 4 6 8 10 12 14 16 18 20
+do
+	echo "Sobel con imagen 720 y Kernel de 3x3 - $ Hilo"
+	echo "messi720.jpg m720-3.jpg 5 $hilos" | ./$EXEC > out720-3.txt
+done
