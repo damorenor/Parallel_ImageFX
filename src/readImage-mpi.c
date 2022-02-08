6248b502d1fa03a329444cc820a2bf8457dab0fe
damorenor <damorenor@unal.edu.co>
2022-02-08 08:20:41 -0500

    missed an *, sorry

--- src/readImage-mpi.c
+++ src/readImage-mpi.c
@@ -52,7 +52,7 @@ int main(int argc, char **argv)
 
     //Carga de imagen usando laS librerias
     unsigned char *img = stbi_load(name, &width, &height, &channels, 3);
-    char *img2, sub_img2;
+    char *img2, *sub_img2;
     img2 = malloc(width * channels* height*sizeof(char));
     sub_img2 = malloc(width * channels* height*sizeof(char) / num_procs);
     int pixel_row_num = width * channels;
